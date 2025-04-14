e5e2bb2b0691b5efac0584f2d169fac34f595b9c
Anton Gubernatorov <gubernatorov.tosha@gmail.com>
2025-04-14 17:52:13 +0300

    Intitialized a patient service

---
+++ patient-service/src/test/java/ru/gubern/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gubern.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
