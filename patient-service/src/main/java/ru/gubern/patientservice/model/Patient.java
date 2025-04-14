5743f97a6763574c428619895afff91f51e7c916
Anton Gubernatorov <gubernatorov.tosha@gmail.com>
2025-04-14 18:41:17 +0300

    Add patient domain layer

--- patient-service/src/main/java/ru/gubern/patientservice/model/Patient.java
+++ patient-service/src/main/java/ru/gubern/patientservice/model/Patient.java
@@ -1,2 +1,39 @@
-package ru.gubern.patientservice.model;public class Patient {
+package ru.gubern.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Data
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredAt;
 }
